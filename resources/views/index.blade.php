a56a8da6c777063ac13c6b1afb9a8779ba7b0bb5
Shah Alam <shahalam141072@gmail.com>
2023-01-14 17:59:33 +0600

    modal page modified

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,7 @@
                                 <th scope="col">#</th>
                                 <th scope="col">Name</th>
                                 <th scope="col">Price</th>
-                                <th scope="col">Category</th>
+                                <th scope="col">Discount</th>
                                 <th scope="col">Action</th>
                             </tr>
                         </thead>
@@ -54,45 +54,9 @@
         </div>
     </div>
 
-    <!-- Modal -->
-    <div class="modal fade" id="showModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <form id="addProduct">
-                    <div class="modal-body">
-                        <div class="card">
-                            <div class="card-body">
-
-                                <div class="mb-3">
-                                    <label for="name" class="form-label">Product Name*</label>
-                                    <input type="text" class="form-control" name="name">
-                                    <div id="errorText" class="form-text">error</div>
-                                </div>
-                                <div class="mb-3">
-                                    <label for="price" class="form-label">Product Price*</label>
-                                    <input type="number" class="form-control" name="price">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="category" class="form-label">Product Category*</label>
-                                    <input type="text" class="form-control" name="category">
-                                </div>
-
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+    <!-- Modal start -->
+    @include('create')
+    <!-- Modal end -->
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
