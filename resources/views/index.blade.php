125c6b60abd1b390c28e0c94920195c067f195c5
Shah Alam <shahalam141072@gmail.com>
2023-01-14 17:44:26 +0600

    modal create for taking input

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,58 +1,105 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 
     <title>Ajax Crud</title>
-  </head>
-  <body>
+</head>
+
+<body>
 
     <div class="container">
         <div class="row mt-5">
             <div class="col-lg-8 m-auto">
                 <div class="table_header my-3">
-                    <a href="#" class="btn btn-primary">Add Product</a>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#showModal">
+                        Add Product
+                    </button>
                 </div>
                 <div class="table">
                     <table class="table table-bordered table-striped">
                         <thead class="text-center">
-                          <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Price</th>
-                            <th scope="col">Category</th>
-                            <th scope="col">Action</th>
-                          </tr>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Price</th>
+                                <th scope="col">Category</th>
+                                <th scope="col">Action</th>
+                            </tr>
                         </thead>
                         <tbody class="text-center">
-                          <tr>
-                            <th>1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                            <td>
-                                <a href="#" class="btn btn-success"><i class="las la-edit"></i></a>
-                                <a href="#" class="btn btn-danger"><i class="las la-trash"></i></a>
-                            </td>
-                          </tr>
+                            <tr>
+                                <th>1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>
+                                    <a href="#" class="btn btn-success"><i class="las la-edit"></i></a>
+                                    <a href="#" class="btn btn-danger"><i class="las la-trash"></i></a>
+                                </td>
+                            </tr>
                         </tbody>
-                      </table>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
 
+    <!-- Modal -->
+    <div class="modal fade" id="showModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form id="addProduct">
+                    <div class="modal-body">
+                        <div class="card">
+                            <div class="card-body">
+
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">Product Name*</label>
+                                    <input type="text" class="form-control" name="name">
+                                    <div id="errorText" class="form-text">error</div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="price" class="form-label">Product Price*</label>
+                                    <input type="number" class="form-control" name="price">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="category" class="form-label">Product Category*</label>
+                                    <input type="text" class="form-control" name="category">
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 
 
 
+</body>
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  </body>
 </html>
